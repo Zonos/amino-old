b263998dff426d26f45e2ae78e9ec013546c9737
Joshua Beitler <hello@joshbeitler.com>
2019-01-02 11:22:50 -0700

    Motion and CardStack

---
+++ src/components/CardStack.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import { Theme } from '../styles/Theme'
+
+const CardStack = styled.div`
+  flex: 1;
+  box-sizing: border-box;
+
+  section {
+    margin-bottom: 50px;
+  }
+`;
--- src/styles/Color.ts
+++ src/styles/Color.ts
@@ -2,4 +2,6 @@ export const Color = {
   primaryColor: '#026CFF',
   lightPrimaryColor: '#368AFF',
   borderColor: 'rgba(0, 0, 0, 0.12)',
+  textColor: '#607d8b',
+  headerTextColor: '#263238',
 }

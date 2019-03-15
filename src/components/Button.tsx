4895e280fd2f019ffead6691a55c4edb49b20ad8
Joshua Beitler <oss@joshbeitler.com>
2019-03-15 14:38:56 -0600

    Buttons styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -22,8 +22,7 @@ const StyledButton = styled.button`
   font-size: 15px;
   transition: all 100ms ease-in-out;
   height: 34px;
-  box-shadow: 0 0 0 1px rgba(88, 106, 218, 0.1), 0 2px 5px 0 rgba(88, 106, 218, 0.08),
-    0 1px 1.5px 0 rgba(0, 0, 0, 0.07), 0 1px 2px 0 rgba(0, 0, 0, 0.08), 0 0 0 0 transparent;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1), 0 2px 4px rgba(0, 0, 0, 0.1);
 
   color: ${(p: any) => (p.primary ? "#fff" : Color.text.light)};
   background: ${(p: any) => (p.primary ? Color.primary.base : "#fff")};
@@ -31,8 +30,7 @@ const StyledButton = styled.button`
   &:hover {
     background: ${(p: any) => (p.primary ? Color.primary.dark : "rgba(0, 0, 0, 0.008)")};
     color: ${(p: any) => (p.primary ? "#fff" : Color.text.light)};
-    box-shadow: 0 0 0 1px rgba(88, 106, 218, 0.1), 0 2px 5px 0 rgba(88, 106, 218, 0.08),
-      0 1px 1.5px 0 rgba(0, 0, 0, 0.07), 0 1px 2px 0 rgba(0, 0, 0, 0.08), 0 0 0 0 transparent;
+    box-shadow: 0 1px 5px rgba(0, 0, 0, 0.1), 0 3px 5px rgba(0, 0, 0, 0.1);
   }
 
   &[disabled] {
